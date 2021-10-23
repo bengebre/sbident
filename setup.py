c3f9b60140b46dd1e32069328c07d4f7479bef89
Ben Engebreth <ben.engebreth@gmail.com>
2021-10-23 07:28:18 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 setuptools.setup(
-    name='SBIdent',
+    name='sbident',
     version='0.1',
     author='Ben Engebreth',
     author_email='ben.engebreth@gmail.com',
     description='A Python library for querying the JPL Small Body Identification API',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/bengebre/SBIdent',
-    project_urls = {'Issues': 'https://github.com/bengebre/SBIdent/issues'},
+    url='https://github.com/bengebre/sbident',
+    project_urls = {'Issues': 'https://github.com/bengebre/sbident/issues'},
     license='MIT',
     packages=['sbident'],
     install_requires=['requests', 'pandas', 'astropy'],
